698a63311abf1c271610f26acef360bc89a6d79c
Kutluhan Ünsal <kutluhan.unsal@hotmail.com>
2024-11-11 20:06:57 +0300

    fixed4

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,7 +2,6 @@ import { projects } from "@/data";
 import React from "react";
 import { PinContainer } from "./ui/PinContainer";
 import { FaLocationArrow } from "react-icons/fa6";
-import Image from "next/image";
 
 const RecentProjects = () => {
   return (
@@ -19,9 +18,9 @@ const RecentProjects = () => {
               <PinContainer title={title} href={link}>
                 <div className="relative flex items-center justify-center sm:w-[570px] w[80vw] sm:h-[40vh] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                   <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                    <Image src="/bg.png" alt="bg-img" />
+                    <img src="/bg.png" alt="bg-img" />
                   </div>
-                  <Image
+                  <img
                     src={img}
                     alt={title}
                     className="z-10 absolute bottom-0"
@@ -41,7 +40,7 @@ const RecentProjects = () => {
                         className="border border-white/0.2 rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
                         style={{ transform: `translateX(-${5 * index * 2}px)` }}
                       >
-                        <Image src={icon} alt={icon} className="p-2" />
+                        <img src={icon} alt={icon} className="p-2" />
                       </div>
                     ))}
                   </div>
